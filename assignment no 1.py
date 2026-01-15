54792be04156916f74e5846e53a2379b99b5b780
SahilMenghani04 <sahil.menghani2004@gmail.com>
2026-01-15 18:41:39 +0530

    Add files via upload

---
+++ assignment no 1.py
@@ -0,0 +1,12 @@
+Num1 = int(input("enter first no:",))
+Num2 = int(input("enter second no:",))
+
+add = (Num1+Num2)
+sub = (Num1-Num2)
+div = (Num1/Num2)
+mult =(Num1*Num2)
+
+print("addition",add)
+print("substraction:",sub)
+print("division:",div)
+print("multiplication:",mult)
