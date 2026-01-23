e6974c1b6fb51b675ee0b8ab46e16d1c640c91b6
SahilMenghani04 <sahil.menghani2004@gmail.com>
2026-01-23 17:28:40 +0530

    Uncomment tasks for checking even/odd and summing numbers

--- assignment no 2.py
+++ assignment no 2.py
@@ -9,8 +9,9 @@ print("thanks for using this program")
 
 
 #Task 2: Sum of Integers from 1 to 50 Using a Loop
-# total_sum = 0
-# for number in range (1,51):
-#     total_sum = total_sum + number
-# print(f"The total sum of Number from 1 to 50 is : {total_sum}")
+ total_sum = 0
+ for number in range (1,51):
+     total_sum = total_sum + number
+ print(f"The total sum of Number from 1 to 50 is : {total_sum}")
+
 
